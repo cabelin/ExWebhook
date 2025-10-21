2fa8d0bacae4006ce69d59c1225fc763b3382903
Cabelin <mmds89@gmail.com>
2025-10-21 16:24:03 -0300

    Revert "Merge pull request #5 from cabelin/feature/openapi-file-generation"
    This reverts commit e4e32199d9f8b045edb1ed9ca8ab6bb16180757b, reversing
    changes made to 3414e511ddbdf64a2b5ad491970feb36fccac97d.

--- lib/ExWebhook/web/router.ex
+++ lib/ExWebhook/web/router.ex
@@ -12,17 +12,4 @@ defmodule ExWebhook.Web.Router do
     get("/webhooks", WebhookController, :index)
     delete("/webhooks/:id", WebhookController, :delete)
   end
-
-  scope "/q/openapi" do
-    forward("/", PhoenixSwagger.Plug.SwaggerUI, otp_app: :webhook, swagger_file: "swagger.json")
-  end
-
-  def swagger_info do
-    %{
-      info: %{
-        version: "1.0.0",
-        title: "Webhook API"
-      }
-    }
-  end
 end
